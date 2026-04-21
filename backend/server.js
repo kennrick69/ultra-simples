b5ed76de2016acc5d63ddfc44dedb7ccb01fd8fd
Jose <kennrick@gmail.com>
2026-04-21 20:04:49 -0300

    Move frontend para backend/frontend e ajusta paths

--- backend/server.js
+++ backend/server.js
@@ -5003,10 +5003,10 @@ app.get('/health', async (req, res) => {
     }
 });
 
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+    res.sendFile(path.join(__dirname, 'frontend/index.html'));
 });
 
 app.use((req, res) => {
